ee9ce990ce4354beb8e6aae73ac9732dddf35e22
parshantbisht656@gmail.com <parshantbisht656@gmail.com>
2024-08-28 22:27:54 +0530

    remove comment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,7 @@ export const Register = async (req, res) => {
 export const Login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        console.log(req.body);
+        
         
         if (!email || !password) {
             return res.status(401).json({
